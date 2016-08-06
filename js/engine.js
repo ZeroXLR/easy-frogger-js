81cda37c9dae083edc9470bfa5c8c1fc6e34b28c
ZeroXLR <ayexlr@gmail.com>
2016-08-06 15:49:08 +0600

    Fix minor typos in engine.js and resources.js

--- js/engine.js
+++ js/engine.js
@@ -17,7 +17,7 @@
 var Engine = (function(global) {
     /* Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
-     * set the canvas elements height/width and add it to the DOM.
+     * set the canvas element's height/width and add it to the DOM.
      */
     var doc = global.document,
         win = global.window,
