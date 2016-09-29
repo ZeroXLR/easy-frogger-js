0687ed242fd00ce1dea9d82d2cd3d691d70668ec
ZeroXLR <ayebuju@gmail.com>
2016-09-29 16:45:16 +0600

    Combine all js files to one to improve loading speed of page; make enemies faster

--- js/engine.js
+++ js/engine.js
@@ -1,5 +1,119 @@
 'use strict';
-/* Engine.js
+/* Resources:
+ * This is simply an image loading utility. It eases the process of loading
+ * image files so that they can be used within your game. It also includes
+ * a simple "caching" layer so it will reuse cached images if you attempt
+ * to load the same image multiple times.
+ */
+(function() {
+	var resourceCache = {};
+	var loading = [];
+	var readyCallbacks = [];
+
+	/* This is the publicly accessible image loading function. It accepts
+	 * an array of strings pointing to image files or a string for a single
+	 * image. It will then call our private image loading function accordingly.
+	 */
+	function load(urlOrArr) {
+		if (urlOrArr instanceof Array) {
+			/* If the developer passed in an array of images
+			 * loop through each value and call our image
+			 * loader on that image file
+			 */
+			urlOrArr.forEach(function(url) {
+				_load(url);
+			});
+		} else {
+			/* The developer did not pass an array to this function,
+			 * assume the value is a string and call our image loader
+			 * directly.
+			 */
+			_load(urlOrArr);
+		}
+	}
+
+	/* This is our private image loader function, it is
+	 * called by the public image loader function.
+	 */
+	function _load(url) {
+		if (resourceCache[url]) {
+			/* If this URL has been previously loaded it will exist within
+			 * our resourceCache array. Just return that image rather than
+			 * re-loading the image.
+			 */
+			return resourceCache[url];
+		} else {
+			/* This URL has not been previously loaded and is not present
+			 * within our cache; we'll need to load this image.
+			 */
+			var img = new Image();
+			img.onload = function() {
+				/* Once our image has properly loaded, add it to our cache
+				 * so that we can simply return this image if the developer
+				 * attempts to load this file in the future.
+				 */
+				resourceCache[url] = img;
+
+				/* Once the image is actually loaded and properly cached,
+				 * call all of the onReady() callbacks we have defined.
+				 */
+				if (isReady()) {
+					readyCallbacks.forEach(function(func) {
+						func();
+					});
+				}
+			};
+
+			/* Set the initial cache value to false, this will change when
+			 * the image's onload event handler is called. Finally, point
+			 * the image's src attribute to the passed in URL.
+			 */
+			resourceCache[url] = false;
+			img.src = url;
+		}
+	}
+
+	/* This is used by developers to grab references to images they know
+	 * have been previously loaded. If an image is cached, this functions
+	 * the same as calling load() on that URL.
+	 */
+	function get(url) {
+		return resourceCache[url];
+	}
+
+	/* This function determines if all of the images that have been requested
+	 * for loading have in fact been properly loaded.
+	 */
+	function isReady() {
+		var ready = true;
+		for (var k in resourceCache) {
+			if (resourceCache.hasOwnProperty(k) &&
+				!resourceCache[k]) {
+				ready = false;
+			}
+		}
+		return ready;
+	}
+
+	/* This function will add a function to the callback stack that is called
+	 * when all requested images are properly loaded.
+	 */
+	function onReady(func) {
+		readyCallbacks.push(func);
+	}
+
+	/* This object defines the publicly accessible functions available to
+	 * developers by creating a global Resources object.
+	 */
+	window.Resources = {
+		load: load,
+		get: get,
+		onReady: onReady,
+		isReady: isReady
+	};
+})();
+
+/* Engine:
  * This file provides the game loop functionality (update entities and render),
  * draws the initial game board on the screen, and then calls the update and
  * render methods on your player and enemy objects (defined in your app.js).
@@ -57,7 +171,6 @@ var Engine = (function(global) {
 		/* Call our update/render functions, pass along the time delta to
 		 * our update function since it may be used for smooth animation.
 		 */
-		// update(dt); // useless since is just calls updateEntities()
 		updateEntities(dt);
 		render();
 		checkCollisions();
@@ -232,3 +345,165 @@ var Engine = (function(global) {
 		canvasheight: ctx.canvas.height,
 	};
 })(this);
+
+// Parent class of both the Enemy and Player classes
+var Entity = (function() {
+	// A couple of private constants (private static variables in Java lingo) and functions of Entity
+	var x_collision_limit = (Engine.allimgwidths / 3) * 2,
+		y_collision_limit = Engine.allimgwidths / 2,
+		ctx = Engine.ctx;
+
+	// x0 and y0 must be instance specific; if it were private static like the variables above, they would change everytime a new Enemy() or a new Player() is made
+	function Entity(sprite, x, y) {
+		this.sprite = sprite;
+		this.x = this.x0 = x;
+		this.y = this.y0 = y;
+	}
+
+	// Makes sure that the child class has a properly structured prototype
+	Entity.passOwnPrototypeTo = function(Child) {
+		Child.prototype = Object.create(Entity.prototype);
+		Child.prototype.constructor = Child;
+	};
+
+	Entity.prototype.render = function() {
+		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	};
+
+	Entity.prototype.restart = function() {
+		this.x = this.x0;
+		this.y = this.y0;
+	}
+
+	Entity.prototype.collidesWith = function(entity) {
+		return entity.x <= (this.x + x_collision_limit) && (this.x - x_collision_limit) <= entity.x && entity.y <= (this.y + y_collision_limit) && (this.y - y_collision_limit) <= entity.y;
+	};
+
+	return Entity;
+})();
+
+// Enemies our player must avoid
+var Enemy = (function() {
+	// A couple of private constants (i.e. private static variables in Java lingo) associated with the Enemy class
+	var canvaswidth = Engine.canvaswidth,
+		pixelGapPerRow = Engine.pixelGapPerRow,
+		min_speed = canvaswidth / 2;
+	var range_speed = canvaswidth;
+
+	function Enemy() {
+		// call the parent constructor under this particular context
+		Entity.call(this, 'images/enemy-bug.png', 0, pixelGapPerRow / 2);
+		this.speed = Math.random() * range_speed + min_speed;
+	}
+
+	Entity.passOwnPrototypeTo(Enemy);
+
+	// Update the enemy's position, required method for game
+	// Parameter: dt, a time delta between ticks
+	Enemy.prototype.update = function(dt) {
+		// You should multiply any movement by the dt parameter
+		// which will ensure the game runs at the same speed for
+		// all computers.
+		this.x = (this.x + dt * this.speed) % canvaswidth;
+	};
+
+	return Enemy;
+})();
+
+var Player = (function() {
+	// A couple of private constants (i.e. private static variables in Java lingo) associated with the Player class
+	var characters = Engine.characters,
+		numCols = Engine.numCols,
+		pixelGapPerCol = Engine.pixelGapPerCol;
+	var stride_x = Engine.pixelGapPerRow / 2,
+		stride_y = pixelGapPerCol / 2,
+		max_x = Engine.canvaswidth - Engine.allimgwidths; // can't go more east than this
+	var max_y = (Engine.numRows - 1) * Engine.pixelGapPerRow; // can't go more south than this
+
+	function Player() {
+		Entity.call(
+			this,
+			characters[Math.floor(Math.random() * characters.length)], // randomly select a character on each instantiation
+			Math.floor(numCols / 2) * pixelGapPerCol, // initial_x = middle_column_index * 101 = 2 * 101
+			max_y // initial_y = last_row_index * 83 = 5 * 83
+		);
+	}
+
+	Entity.passOwnPrototypeTo(Player);
+
+	Player.prototype.reachedWater = function() {
+		return this.y === 0;
+	};
+
+	Player.prototype.handleInput = function(key) {
+		var newpos;
+		switch (key) {
+			case 'left':
+				this.x = (newpos = this.x - stride_x) > 0 ? newpos : 0;
+				break;
+			case 'up':
+				this.y = (newpos = this.y - stride_y) > 0 ? newpos : 0;
+				break;
+			case 'right':
+				this.x = (newpos = this.x + stride_x) < max_x ? newpos : max_x;
+				break;
+			case 'down':
+				this.y = (newpos = this.y + stride_y) < max_y ? newpos : max_y;
+				break;
+		}
+	};
+
+	return Player;
+})();
+
+// A factory to create enemies
+function enemyFactory(number) {
+	if (number > 6) number = 6; // or else the enemies will just overlap which is pretty much the same as having a single enemy
+	var enemies = [],
+		enemy;
+	// From 6 possible locations randomly choose 'number' locations WITHOUT replacement
+	(function(array) {
+		var currentIndex = array.length,
+			temporaryValue,
+			randomIndex,
+			finalarray = [], // array to store only as many elements as needed
+		// While finalarray does not contain as many elements as required...
+			count;
+
+		for (var count = 0; count < number; ++count) {
+			// Pick a remaining element...
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			--currentIndex;
+			finalarray.push(array[randomIndex]);
+
+			// And swap it with the current element.
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+		return finalarray;
+	})([1, 2, 3, 4, 5, 6]).sort(function(a, b) { // sorting makes sure that the enemies are added according to perpective i.e. further enemies seem to be behind closer ones
+			return a - b;
+		}).forEach(function(element) {
+			enemy = new Enemy();
+			enemy.y *= element;
+			enemies.push(enemy);
+		});
+	return enemies;
+}
+var allEnemies = enemyFactory(4);
+var player = new Player();
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method.
+document.addEventListener('keydown', function(e) {
+	var allowedKeys = {
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down'
+	};
+
+	player.handleInput(allowedKeys[e.keyCode]);
+});
